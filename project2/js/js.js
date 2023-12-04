f8b500e8538f5a32c83271c778b2aa7ecfb12202
seobyeonggwon <146168751+seobyeonggwon@users.noreply.github.com>
2023-12-04 14:16:13 +0900

    1204_update

---
+++ project2/js/js.js
@@ -0,0 +1,12 @@
+// 제이쿼리방식으로 시작할지 
+// 라이브러리를 사용하지않고 사용할지를 결정하세요
+
+
+// $(function(){
+//     $('.txt').mousemove(function(e){
+//         let x = e.pageX;
+//         // let wid = $().width();
+//         // let xwid = Math.floor((x / wid) * 200);
+//         $('em').text(x);
+//     })
+// })
